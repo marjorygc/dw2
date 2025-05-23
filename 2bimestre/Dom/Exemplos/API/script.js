5ed6f101c12f5b4725feb500bfe09ce3740dd8f0
marjorygc <163425550+marjorygc@users.noreply.github.com>
2025-05-23 15:30:45 -0300

    new stuff

---
+++ 2bimestre/Dom/Exemplos/API/script.js
@@ -0,0 +1,28 @@
+function buscarCep(){
+    const cep = document.getElementById("cep").value;
+
+    const url = `https://viacep.com.br/ws/${cep}/json`;
+
+    fetch(url)
+        .then((response) => {
+                return response.json()
+        })
+        .then((data) => {
+            if(data.erro){
+                alert("CEP não encontrado")
+                return
+            }
+
+            document.getElementById("logradouro").value = data.logradouro;
+            document.getElementById("bairro").value = data.bairro;
+            document.getElementById("cidade").value = data.localidade;
+            document.getElementById("estado").value = data.estado + ` (${data.uf})`;
+
+        })
+
+        . catch((eeror) => {
+            alert("Erro ao buscar o CEP!")
+        })        
+
+
+}
