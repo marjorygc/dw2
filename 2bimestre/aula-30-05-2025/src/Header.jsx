509146e43a9c3b992f20aab0c449c1af719af5ec
marjorygc <163425550+marjorygc@users.noreply.github.com>
2025-05-30 11:37:40 -0300

    inicio da tarefa

---
+++ 2bimestre/aula-30-05-2025/src/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css'
+
+function Header(){
+    return (
+        <header className='app-header'>
+
+            <h1>Bem-vido à página com React</h1>
+        </header>
+    )
+}
+
+export default Header;
