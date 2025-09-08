5d14065e1eb73a8ac1161f52dad953c52c0c9d51
marjorygc <163425550+marjorygc@users.noreply.github.com>
2025-09-08 09:38:01 -0300

    aula useState

--- 3bimestre/contador/src/components/Contador.jsx
+++ 3bimestre/contador/src/components/Contador.jsx
@@ -1,14 +1,36 @@
+import { useState } from "react";
 import styles from"./Contador.module.css"
 
 export default function Contador(){
+
+  //const estado = useState[0]
+  //let contador = estado[0]
+  //const setContador = estado[1]
+
+  let [contador, setContador] = useState(0)
+
+  function incrementar(){
+    setContador(contador => contador + 1)
+  }
+
+  function decrementar(){
+   setContador(contador => contador - 1)
+  }
+
   return(
     <>
       <div className={styles.container}>
         <h1>Contador</h1>
-        <div className={styles.contadorDisplay}>0</div>
+        <div className={styles.contadorDisplay}>{contador}</div>
         <div className={styles.botaoContainer}>
-          <button className={styles.botao}>-</button>
-          <button className={styles.botao}>+</button>
+          <button
+           className={styles.botao}
+           onClick={decrementar}
+           >-</button>
+          <button 
+           className={styles.botao}
+          onClick={incrementar}
+          >+</button>
         </div>
         <button className={styles.botao}> Reset</button>
       </div>
